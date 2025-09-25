7cb0db8e7b66593c2888d1e69c0ed9a1f91d4a37
ExBrain <zecarlosinf@gmail.com>
2025-09-25 22:47:12 +0100

    Changed progress to circular bar

--- flutter-gstracker/lib/screens/home_screen/widgets/home_player_progress.dart
+++ flutter-gstracker/lib/screens/home_screen/widgets/home_player_progress.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:tracker/common/extensions/extensions.dart';
 import 'package:tracker/common/lang/lang.dart';
 import 'package:tracker/common/widgets/cards/gs_data_box.dart';
+import 'package:tracker/common/widgets/static/sliver_grid_fixed_size_delegate.dart';
 import 'package:tracker/common/widgets/static/value_stream_builder.dart';
 import 'package:tracker/domain/gs_database.dart';
 import 'package:tracker/theme/gs_assets.dart';
@@ -17,56 +18,70 @@ class HomePlayerProgress extends StatelessWidget {
       builder: (context, snapshot) {
         return GsDataBox.info(
           title: Text(context.labels.progress()),
-          children: <Widget>[
-            _ProgressInfo(
-              AppAssets.menuIconAchievements,
-              context.labels.achievements(),
-              GsUtils.achievements.countSaved(),
-              GsUtils.achievements.countTotal(),
+          child: Padding(
+            padding: EdgeInsetsGeometry.only(top: kSeparator8),
+            child: GridView.custom(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedChildSize(
+                childWidth: 115,
+                childHeight: 115,
+                mainAxisSpacing: GsSpacing.kGridSeparator,
+                crossAxisSpacing: GsSpacing.kGridSeparator,
+                alignment: CrossAxisAlignment.center,
+              ),
+              childrenDelegate: SliverChildListDelegate([
+                _ProgressInfo(
+                  AppAssets.menuIconAchievements,
+                  context.labels.achievements(),
+                  GsUtils.achievements.countSaved(),
+                  GsUtils.achievements.countTotal(),
+                ),
+                _ProgressInfo(
+                  AppAssets.menuIconRecipes,
+                  context.labels.recipes(),
+                  GsUtils.recipes.totalPermanent(owned: true),
+                  GsUtils.recipes.totalPermanent(),
+                ),
+                _ProgressInfo(
+                  AppAssets.menuIconRecipes,
+                  context.labels.filterProficiency(),
+                  GsUtils.recipes.totalMastered(owned: true),
+                  GsUtils.recipes.totalMastered(),
+                ),
+                _ProgressInfo(
+                  AppAssets.menuIconMap,
+                  context.labels.remarkableChests(),
+                  GsUtils.furnitureChests.owned,
+                  GsUtils.furnitureChests.total,
+                ),
+                _ProgressInfo(
+                  AppAssets.menuEnvisagedEchoes,
+                  context.labels.envisagedEchoes(),
+                  GsUtils.envisagedEchos.owned,
+                  GsUtils.envisagedEchos.total,
+                ),
+                _ProgressInfo(
+                  AppAssets.menuIconThespianTricks,
+                  context.labels.thespianTricks(),
+                  GsUtils.thespianTricks.owned,
+                  GsUtils.thespianTricks.total,
+                ),
+                _ProgressInfo(
+                  AppAssets.menuIconPreciousItems,
+                  context.labels.spincrystals(),
+                  GsUtils.spincrystals.owned,
+                  GsUtils.spincrystals.total,
+                ),
+                _ProgressInfo(
+                  AppAssets.menuIconSereniteaSets,
+                  context.labels.sereniteaSets(),
+                  GsUtils.sereniteaSets.owned,
+                  GsUtils.sereniteaSets.total,
+                ),
+              ]),
             ),
-            _ProgressInfo(
-              AppAssets.menuIconRecipes,
-              context.labels.recipes(),
-              GsUtils.recipes.totalPermanent(owned: true),
-              GsUtils.recipes.totalPermanent(),
-            ),
-            _ProgressInfo(
-              AppAssets.menuIconRecipes,
-              context.labels.filterProficiency(),
-              GsUtils.recipes.totalMastered(owned: true),
-              GsUtils.recipes.totalMastered(),
-            ),
-            _ProgressInfo(
-              AppAssets.menuIconMap,
-              context.labels.remarkableChests(),
-              GsUtils.furnitureChests.owned,
-              GsUtils.furnitureChests.total,
-            ),
-            _ProgressInfo(
-              AppAssets.menuEnvisagedEchoes,
-              context.labels.envisagedEchoes(),
-              GsUtils.envisagedEchos.owned,
-              GsUtils.envisagedEchos.total,
-            ),
-            _ProgressInfo(
-              AppAssets.menuIconThespianTricks,
-              context.labels.thespianTricks(),
-              GsUtils.thespianTricks.owned,
-              GsUtils.thespianTricks.total,
-            ),
-            _ProgressInfo(
-              AppAssets.menuIconPreciousItems,
-              context.labels.spincrystals(),
-              GsUtils.spincrystals.owned,
-              GsUtils.spincrystals.total,
-            ),
-            _ProgressInfo(
-              AppAssets.menuIconSereniteaSets,
-              context.labels.sereniteaSets(),
-              GsUtils.sereniteaSets.owned,
-              GsUtils.sereniteaSets.total,
-            ),
-          ].separate(const SizedBox(height: kSeparator16)).toList(),
+          ),
         );
       },
     );
@@ -88,73 +103,63 @@ class _ProgressInfo extends StatelessWidget {
 
     final style = context.themeStyles.label12n;
 
-    return Row(
+    return Column(
+      spacing: kSeparator4,
+      mainAxisSize: MainAxisSize.min,
       children: [
-        Image.asset(asset, width: 46, height: 46),
-        const SizedBox(width: kSeparator8),
-        Expanded(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                children: [
-                  Expanded(child: Text(label)),
-                  Text(
-                    owned.format(),
-                    textAlign: TextAlign.end,
-                    style: style.copyWith(
-                      color: missing > 0
-                          ? context.themeColors.badValue
-                          : Colors.white,
-                    ),
-                  ),
-                  if (missing > 0)
-                    Text(
-                      ' +${missing.format()}',
-                      style: style.copyWith(
-                        fontStyle: FontStyle.italic,
-                        fontSize: 10,
-                        color: context.themeColors.goodValue,
-                      ),
-                      strutStyle: style.copyWith(fontSize: 10).toStrut(),
-                    ),
-                  Text(
-                    ' / ${total.format()}',
-                    textAlign: TextAlign.end,
-                    style: style,
-                  ),
-                ],
+        Stack(
+          children: [
+            SizedBox(
+              width: 46,
+              height: 46,
+              child: Center(child: Image.asset(asset, width: 32, height: 32)),
+            ),
+            CircularProgressIndicator(
+              constraints: BoxConstraints.tight(Size(46, 46)),
+              value: percentage,
+              strokeWidth: 4,
+              backgroundColor: context.themeColors.mainColor1,
+              color: context.themeColors.almostWhite,
+              strokeCap: StrokeCap.round,
+            ),
+          ],
+        ),
+        SizedBox(height: kSeparator2),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              owned.format(),
+              textAlign: TextAlign.end,
+              style: style.copyWith(
+                color: missing > 0
+                    ? context.themeColors.badValue
+                    : Colors.white,
               ),
-              const SizedBox(height: kSeparator8),
-              _progressBar(context, percentage),
-            ],
-          ),
+            ),
+            if (missing > 0)
+              Text(
+                ' +${missing.format()}',
+                style: style.copyWith(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 10,
+                  color: context.themeColors.goodValue,
+                ),
+                strutStyle: style.copyWith(fontSize: 10).toStrut(),
+              ),
+            Text(
+              ' / ${total.format()}',
+              textAlign: TextAlign.end,
+              style: style,
+            ),
+          ],
+        ),
+        Text(
+          label,
+          textAlign: TextAlign.center,
+          style: context.themeStyles.label12n,
         ),
       ],
-    );
-  }
-
-  Widget _progressBar(BuildContext context, double percentage) {
-    return Container(
-      height: kSeparator6,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: context.themeColors.mainColor1,
-        borderRadius: BorderRadius.circular(4),
-      ),
-      child: AnimatedFractionallySizedBox(
-        duration: const Duration(milliseconds: 400),
-        curve: Curves.easeInOut,
-        widthFactor: percentage,
-        alignment: Alignment.centerLeft,
-        child: Container(
-          height: 4,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(4),
-          ),
-        ),
-      ),
     );
   }
 }
